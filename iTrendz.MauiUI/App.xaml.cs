7f3037c70abfd1c5314a51e6912764967c0131ef
Cristian Nita <cristiannita24@gmail.com>
2024-04-29 18:16:56 +0300

    Added Maui User Interface Project

---
+++ iTrendz.MauiUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace iTrendz.MauiUI;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ iTrendz.MauiUI/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace iTrendz.MauiUI;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
