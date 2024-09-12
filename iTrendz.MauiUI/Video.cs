50ebf6715f1cdb646ce3491c5d266e72ad308545
danielbunea68 <69964215+danielbunea68@users.noreply.github.com>
2024-09-12 15:09:08 +0300

    am recentrat buttoanele de la insights

---
+++ iTrendz.MauiUI/Video.cs
@@ -0,0 +1,9 @@
+﻿namespace iTrendz.MauiUI;
+
+public class Video
+{
+	public int Id { get; set; }	
+	public string? Title {  get; set; }	
+	public string? VideoUrl { get; set; }
+	public string? PosterUrl { get; set; }
+}
