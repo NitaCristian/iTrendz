2e1eafe753b90627320ff66ba3a566bd0b054945
danielbunea68 <69964215+danielbunea68@users.noreply.github.com>
2024-09-17 15:33:46 +0300

    Removed old files from API and MAUI project

--- iTrendz.MauiUI/Video.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace iTrendz.MauiUI;
-
-public class Video
-{
-	public int Id { get; set; }	
-	public string? Title {  get; set; }	
-	public string? VideoUrl { get; set; }
-	public string? PosterUrl { get; set; }
-}
