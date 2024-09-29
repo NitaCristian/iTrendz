7980f76d60a2feb03a6aef8c837f2a77a2d75018
Cristian Nita <cristiannita24@gmail.com>
2024-09-29 14:08:47 +0300

    Created the Influencer Profile page

--- iTrendz.Domain/Models/Video.cs
+++ iTrendz.Domain/Models/Video.cs
@@ -2,6 +2,18 @@
 
 public class Video
 {
+	public Video()
+	{
+	}
+
+	public Video(int id, string? title, string? videoUrl, string? posterUrl)
+	{
+		Id = id;
+		Title = title;
+		VideoUrl = videoUrl;
+		PosterUrl = posterUrl;
+	}
+
 	public int Id { get; set; }	
 	public string? Title {  get; set; }	
 	public string? VideoUrl { get; set; }
