94531cae61e1c7309bac0ddb8d37d5e2ad1d1ad2
Cristian Nita <cristiannita24@gmail.com>
2024-10-01 12:10:54 +0300

    Extracted components and refactored the BrandCampaignDetails

---
+++ iTrendz.Domain/Models/BudgetStatistics.cs
@@ -0,0 +1,18 @@
+﻿namespace iTrendz.Domain.Models;
+
+public class BudgetStatistics
+{
+    public List<ChartSeries> BudgetHistoryData { get; set; }
+    public string[] BudgetHistoryLabels { get; set; }
+
+    public double[] BudgetAllocationData { get; set; }
+    public string[] BudgetAllocationLabels { get; set; }
+}
+
+public class ChartSeries
+{
+    public string Name { get; set; }
+    public double[] Data { get; set; }
+    public bool Visible { get; set; } = true;
+    public int Index { get; set; }
+}
---
+++ iTrendz.Domain/Models/TimelineEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace iTrendz.Domain.Models;
+
+public class TimelineEvent
+{
+    public string Title { get; set; }
+    public DateTime Timestamp { get; set; }
+    public int ColorCode { get; set; }
+}
