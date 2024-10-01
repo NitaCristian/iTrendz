576c1d00f752e044c622ae44785588acd5eba9f4
Cristian Nita <cristiannita24@gmail.com>
2024-10-01 12:41:11 +0300

    Completed the budget statistics component

--- iTrendz.Domain/Models/BudgetStatistics.cs
+++ iTrendz.Domain/Models/BudgetStatistics.cs
@@ -2,17 +2,9 @@
 
 public class BudgetStatistics
 {
-    public List<ChartSeries> BudgetHistoryData { get; set; }
+    public double[] BudgetHistoryData { get; set; }
     public string[] BudgetHistoryLabels { get; set; }
 
     public double[] BudgetAllocationData { get; set; }
     public string[] BudgetAllocationLabels { get; set; }
-}
-
-public class ChartSeries
-{
-    public string Name { get; set; }
-    public double[] Data { get; set; }
-    public bool Visible { get; set; } = true;
-    public int Index { get; set; }
 }
