5da05bdd5ee55fb8838b64223052c2840c8d4a2c
Cristian Nita <cristiannita24@gmail.com>
2024-10-03 22:56:05 +0300

    Integrated Timeline into Campaign

--- iTrendz.Domain/Models/TimelineEvent.cs
+++ iTrendz.Domain/Models/TimelineEvent.cs
@@ -1,8 +1,17 @@
 ﻿namespace iTrendz.Domain.Models;
 
-public class TimelineEvent
+public class TimelineEvent(string title, string description, DateTime timestamp, EventType type)
 {
-    public string Title { get; set; }
-    public DateTime Timestamp { get; set; }
-    public int ColorCode { get; set; }
+    public string Title { get; set; } = title;
+    public string Description { get; set; } = description;
+    public DateTime Timestamp { get; set; } = timestamp;
+    public EventType Type { get; set; } = type;
+}
+
+public enum EventType
+{
+    Info,
+    Positive,
+    Negative,
+    Warning,
 }
