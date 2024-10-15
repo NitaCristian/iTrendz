356e1bc3998864efafb02933982ce0ba6a0b62a6
danielbunea68 <69964215+danielbunea68@users.noreply.github.com>
2024-10-15 17:39:54 +0300

    Updated Brand service

--- iTrendz.Domain/Interfaces/IBrandService.cs
+++ iTrendz.Domain/Interfaces/IBrandService.cs
@@ -5,7 +5,7 @@ namespace iTrendz.Domain.Interfaces;
 public interface IBrandService
 {
     Task<Brand?> GetBrandByIdAsync(int brandId);
-    Task<IEnumerable<Brand>> GetAllBrandsAsync();
+    Task<IEnumerable<Brand>?> GetAllBrandsAsync();
     Task UpdateBrandAsync(int brandId, Brand updatedBrand);
     Task DeleteBrandAsync(int brandId);
 }
--- iTrendz.MauiUI/Services/BrandService.cs
+++ iTrendz.MauiUI/Services/BrandService.cs
@@ -7,36 +7,32 @@ namespace iTrendz.MauiUI.Services;
 
 public class BrandService(HttpClient httpClient) : IBrandService
 {
-    public async Task<Brand?> GetBrandByIdAsync(int brandId)
-    {
+	public async Task<Brand?> GetBrandByIdAsync(int brandId)
+	{
 
-		var response = await httpClient.GetAsync($"user/{brandId}");
+		var response = await httpClient.GetAsync($"brand/{brandId}");
 		if (!response.IsSuccessStatusCode) return null;
-        return await response.Content.ReadFromJsonAsync<Brand>();
+		return await response.Content.ReadFromJsonAsync<Brand>();
 	}
 
-    public async Task<IEnumerable<Brand>> GetAllBrandsAsync()
-    {
+	public async Task<IEnumerable<Brand>?> GetAllBrandsAsync()
+	{
 		var response = await httpClient.GetAsync("brand/all");
 		if (!response.IsSuccessStatusCode) return new List<Brand>();
 		return await response.Content.ReadFromJsonAsync<List<Brand>>();
 	}
 
-    public async Task UpdateBrandAsync(int brandId, Brand updatedBrand)
-    {
+	public async Task UpdateBrandAsync(int brandId, Brand updatedBrand)
+	{
 		var response = await httpClient.PutAsJsonAsync($"brand/{brandId}", updatedBrand);
-
 		if (!response.IsSuccessStatusCode)
-			throw new Exception($"faild to upadate brand");
-		
+			throw new Exception($"Failed to upadate brand.");
 	}
 
-    public async Task DeleteBrandAsync(int brandId)
-    {
+	public async Task DeleteBrandAsync(int brandId)
+	{
 		var response = await httpClient.DeleteAsync($"brand/{brandId}");
-
 		if (!response.IsSuccessStatusCode)
-			throw new Exception($"faild to upadate influencer");
-
+			throw new Exception($"Failed to delete brand.");
 	}
 }
