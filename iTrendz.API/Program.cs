2562a1f867c01b0e71cc032e18ed4babc33b7353
danielbunea68 <69964215+danielbunea68@users.noreply.github.com>
2024-10-18 15:07:44 +0300

    Updated API services

--- iTrendz.API/Program.cs
+++ iTrendz.API/Program.cs
@@ -9,6 +9,7 @@ using iTrendz.API.Repositories;
 using iTrendz.Domain.Context;
 using iTrendz.Domain.Fakers;
 using iTrendz.Domain.Models;
+using iTrendz.Domain.Interfaces;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,43 +18,43 @@ var services = builder.Services;
 services.AddControllers();
 
 services.AddDbContext<TrendzDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+	options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 services.AddIdentity<User, IdentityRole<int>>(options => options.User.AllowedUserNameCharacters += " ")
-    .AddEntityFrameworkStores<TrendzDbContext>()
-    .AddDefaultTokenProviders();
+	.AddEntityFrameworkStores<TrendzDbContext>()
+	.AddDefaultTokenProviders();
 
 services.AddIdentityCore<Brand>()
-    .AddEntityFrameworkStores<TrendzDbContext>();
+	.AddEntityFrameworkStores<TrendzDbContext>();
 
 services.AddIdentityCore<Influencer>()
-    .AddEntityFrameworkStores<TrendzDbContext>();
+	.AddEntityFrameworkStores<TrendzDbContext>();
 
-services.AddScoped<CampaignRepository>();
-services.AddScoped<ContractRepository>();
+services.AddScoped<ICampaignRepository, CampaignRepository>();
+services.AddScoped<IContractRepository, ContractRepository>();
 
 
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen(options =>
 {
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please enter 'Bearer [jwt]'",
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
-    });
+	options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		In = ParameterLocation.Header,
+		Description = "Please enter 'Bearer [jwt]'",
+		Name = "Authorization",
+		Type = SecuritySchemeType.ApiKey
+	});
 
-    var scheme = new OpenApiSecurityScheme
-    {
-        Reference = new OpenApiReference
-        {
-            Type = ReferenceType.SecurityScheme,
-            Id = "Bearer"
-        }
-    };
+	var scheme = new OpenApiSecurityScheme
+	{
+		Reference = new OpenApiReference
+		{
+			Type = ReferenceType.SecurityScheme,
+			Id = "Bearer"
+		}
+	};
 
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement { { scheme, Array.Empty<string>() } });
+	options.AddSecurityRequirement(new OpenApiSecurityRequirement { { scheme, Array.Empty<string>() } });
 });
 
 using var loggerFactory = LoggerFactory.Create(b => b.SetMinimumLevel(LogLevel.Trace).AddConsole());
@@ -61,40 +62,40 @@ using var loggerFactory = LoggerFactory.Create(b => b.SetMinimumLevel(LogLevel.T
 var secret = builder.Configuration["JWT:Secret"] ?? throw new InvalidOperationException("Secret not configured");
 
 services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        options.SaveToken = true;
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
-            ValidAudience = builder.Configuration["JWT:ValidAudience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
-            ClockSkew = new TimeSpan(0, 0, 5)
-        };
-        options.Events = new JwtBearerEvents
-        {
-            OnChallenge = ctx => LogAttempt(ctx.Request.Headers, "OnChallenge"),
-            OnTokenValidated = ctx => LogAttempt(ctx.Request.Headers, "OnTokenValidated")
-        };
-    });
+	{
+		options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+		options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+		options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+	})
+	.AddJwtBearer(options =>
+	{
+		options.SaveToken = true;
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+			ValidAudience = builder.Configuration["JWT:ValidAudience"],
+			IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
+			ClockSkew = new TimeSpan(0, 0, 5)
+		};
+		options.Events = new JwtBearerEvents
+		{
+			OnChallenge = ctx => LogAttempt(ctx.Request.Headers, "OnChallenge"),
+			OnTokenValidated = ctx => LogAttempt(ctx.Request.Headers, "OnTokenValidated")
+		};
+	});
 
 const string policy = "defaultPolicy";
 
 services.AddCors(options =>
 {
-    options.AddPolicy(policy,
-        p =>
-        {
-            p.AllowAnyHeader();
-            p.AllowAnyMethod();
-            p.AllowAnyHeader();
-            p.AllowAnyOrigin();
-        });
+	options.AddPolicy(policy,
+		p =>
+		{
+			p.AllowAnyHeader();
+			p.AllowAnyMethod();
+			p.AllowAnyHeader();
+			p.AllowAnyOrigin();
+		});
 });
 
 var app = builder.Build();
@@ -102,8 +103,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -119,21 +120,21 @@ app.Run();
 
 Task LogAttempt(IHeaderDictionary headers, string eventType)
 {
-    var logger = loggerFactory.CreateLogger<Program>();
+	var logger = loggerFactory.CreateLogger<Program>();
 
-    var authorizationHeader = headers["Authorization"].FirstOrDefault();
+	var authorizationHeader = headers["Authorization"].FirstOrDefault();
 
-    if (authorizationHeader is null)
-        logger.LogInformation($"{eventType}. JWT not present");
-    else
-    {
-        string jwtString = authorizationHeader.Substring("Bearer ".Length);
+	if (authorizationHeader is null)
+		logger.LogInformation($"{eventType}. JWT not present");
+	else
+	{
+		string jwtString = authorizationHeader.Substring("Bearer ".Length);
 
-        var jwt = new JwtSecurityToken(jwtString);
+		var jwt = new JwtSecurityToken(jwtString);
 
-        logger.LogInformation(
-            $"{eventType}. Expiration: {jwt.ValidTo.ToLongTimeString()}. System time: {DateTime.UtcNow.ToLongTimeString()}");
-    }
+		logger.LogInformation(
+			$"{eventType}. Expiration: {jwt.ValidTo.ToLongTimeString()}. System time: {DateTime.UtcNow.ToLongTimeString()}");
+	}
 
-    return Task.CompletedTask;
+	return Task.CompletedTask;
 }
